e42a32f91fddf872acf02ed9c402b45754897a44
Francesca Obordo <fobordo@gmail.com>
2022-01-19 15:43:32 -0500

    Completed code to map GeoJSON points.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,29 +4,32 @@ console.log("working");
 // Create the map object with center and zoom level.
 let map = L.map("mapid").setView([30, 30], 2);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            // The coordinates appear in reverse order, compared to their order in the setView() method. 
-            // This is b/c the GeoJSON data coordinates are set with the first parameter as X (longitude) 
-            // and the second parameter as Y (latitude). The L.geoJSON() layer reverses the coordinates to 
-            // plot them on the map.
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map("mapid").setView([37.5, -122.5], 10);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             // The coordinates appear in reverse order, compared to their order in the setView() method. 
+//             // This is b/c the GeoJSON data coordinates are set with the first parameter as X (longitude) 
+//             // and the second parameter as Y (latitude). The L.geoJSON() layer reverses the coordinates to 
+//             // plot them on the map.
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
 // // Without markers
 // // Get the GeoJSON data.
@@ -45,22 +48,22 @@ let sanFranAirport =
 // })
 // .addTo(map);
 
-// Using the onEachFeature function
-// Get the GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    // Turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-        console.log(layer)
-        layer.bindPopup(
-            `<h3> Airport code: ${feature.properties.faa} </h3> 
-            <hr> 
-            <h3> Airport name: ${feature.properties.name} </h3>`
-        )
-    }
-})
-.addTo(map);
+// // Using the onEachFeature function
+// // Get the GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     // Turn each feature into a marker on the map.
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer)
+//         layer.bindPopup(
+//             `<h3> Airport code: ${feature.properties.faa} </h3> 
+//             <hr> 
+//             <h3> Airport name: ${feature.properties.name} </h3>`
+//         )
+//     }
+// })
+// .addTo(map);
 
-// Change the map style to "satellite-streets-v11."
+// Create the tile layer that will be the background of the map.
 let streets = L.tileLayer(
     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -68,8 +71,29 @@ let streets = L.tileLayer(
         accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+// Add the 'streets' tile layer to the map.
 streets.addTo(map);
 
+// Having the tileLayer() method before accessing large datasets ensures that the map gets loaded 
+// before the data is added to it.
+
 // Access the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/francescaobordo/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/fobordo/Mapping_Earthquakes/main/majorAirports.json";
+
+// Get the GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data)
+    // Create a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        // Turn each feature into a marker on the map.
+        onEachFeature: function(feature, layer) {
+            console.log(layer)
+            layer.bindPopup(
+                `<h3> Airport code: ${feature.properties.faa} </h3> 
+                <hr> 
+                <h3> Airport name: ${feature.properties.name} </h3>`
+            )
+        }
+    })
+    .addTo(map)
+});
