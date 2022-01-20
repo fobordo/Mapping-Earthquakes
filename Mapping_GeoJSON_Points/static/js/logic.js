7186762097e0c958b871eaee10d2813d95dac7eb
Francesca Obordo <fobordo@gmail.com>
2022-01-19 20:35:40 -0500

    Created a control for two maps.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,69 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-let map = L.map("mapid").setView([30, 30], 2);
-
-// // Create the map object with center at the San Francisco airport.
-// let map = L.map("mapid").setView([37.5, -122.5], 10);
-
-// // Add GeoJSON data.
-// let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-//     "type":"Feature",
-//     "properties":{
-//         "id":"3469",
-//         "name":"San Francisco International Airport",
-//         "city":"San Francisco",
-//         "country":"United States",
-//         "faa":"SFO",
-//         "icao":"KSFO",
-//         "alt":"13",
-//         "tz-offset":"-8",
-//         "dst":"A",
-//         "tz":"America/Los_Angeles"},
-//         "geometry":{
-//             "type":"Point",
-//             // The coordinates appear in reverse order, compared to their order in the setView() method. 
-//             // This is b/c the GeoJSON data coordinates are set with the first parameter as X (longitude) 
-//             // and the second parameter as Y (latitude). The L.geoJSON() layer reverses the coordinates to 
-//             // plot them on the map.
-//             "coordinates":[-122.375,37.61899948120117]}}
-// ]};
-
-// // Without markers
-// // Get the GeoJSON data.
-// l.geoJSON(sanFranAirport).addTo(map);
-
-// // Using the pointToLayer function
-// // Get the GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-//     // Turn each feature into a marker on the map.
-//     pointToLayer: function(feature, latlng) {
-//         console.log(feature)
-//         return L.marker(latlng)
-//         // Add a popup marker
-//         .bindPopup('<h2>' + feature.properties.city + '</h2>')
-//     }
-// })
-// .addTo(map);
-
-// // Using the onEachFeature function
-// // Get the GeoJSON data.
-// L.geoJSON(sanFranAirport, {
-//     // Turn each feature into a marker on the map.
-//     onEachFeature: function(feature, layer) {
-//         console.log(layer)
-//         layer.bindPopup(
-//             `<h3> Airport code: ${feature.properties.faa} </h3> 
-//             <hr> 
-//             <h3> Airport name: ${feature.properties.name} </h3>`
-//         )
-//     }
-// })
-// .addTo(map);
-
-// Create the tile layer that will be the background of the map.
+// Create the street view tile layer that will be the default background of the map.
 let streets = L.tileLayer(
     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -71,11 +9,36 @@ let streets = L.tileLayer(
         accessToken: API_KEY
 });
 
-// Add the 'streets' tile layer to the map.
-streets.addTo(map);
+// Create the dark view tile layer that will be an option for the map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Having the tileLayer() method before accessing large datasets ensures that the map gets loaded 
-// before the data is added to it.
+// Create a base layer that holds both maps.
+let baseMaps = {
+    // "Street" and "Dark" set the text on the control of the map
+    // "streets" and "dark" reference the tile layers
+    Street: streets,
+    Dark: dark
+};
+
+// // setView() method
+// // Create the map object with center and zoom level.
+// let map = L.map("mapid").setView([30, 30], 2);
+
+// alternative to setView() method
+// Create the map object with center, zoom level, and default layer.
+let map = L.map("mapid", {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets],
+});
+
+// Pass the map layers into the layers control and add the layers.
+// baseMaps is the base layer object.
+L.control.layers(baseMaps).addTo(map)
 
 // Access the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/fobordo/Mapping_Earthquakes/main/majorAirports.json";
