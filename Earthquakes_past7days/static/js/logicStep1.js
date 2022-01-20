2b56e87648fb1dc584e001aa6c160ade97fa6966
Francesca Obordo <fobordo@gmail.com>
2022-01-20 14:42:43 -0500

    Adding files to map earthquakes from the past 7 days.

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,50 @@
+// Create a map with all recorded earthquakes from the past seven days.
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the street view tile layer that will be the default background of the map.
+let streets = L.tileLayer(
+    'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+});
+
+// Create the satellite street view tile layer that will be an option for the map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+};
+
+// Create the map object with center, zoom level, and default layer.
+let map = L.map("mapid", {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Pass the map layers into the layers control and add the layers.
+// baseMaps is the base layer object.
+L.control.layers(baseMaps).addTo(map)
+
+// // Create a style for the polygons.
+// let myStyle = {
+//     color: "#0000ff",
+//     fillColor: "#ffffa1",
+//     weight: 1
+// }
+
+// Retrieve the earthquake GeoJSON Data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // Create a GeoJSON layer with the retrieved data.
+    L.geoJSON(data)
+    .addTo(map)
+});
